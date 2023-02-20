be14ab246edc46263f6b4d4927058d77e1be92ce
themedoed <93190084+themedoed@users.noreply.github.com>
2023-02-20 22:44:04 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
 let screen = document.getElementById("screen");
-let res = 1;
+let res;
+let flag = null;
+screen.innerHTML = null;
 
-function onClickPlus (value) {
-    res = 10 * res + value;
-    screen.innerHTML = res;
+function onClickAC() {
+    screen.innerHTML = null;
+}
+
+function onClickNum (value) {
+    if (screen.innerHTML == null) {
+        screen.innerHTML = screen.innerHTML + value;
+    }
+    else {
+        screen.innerHTML = screen.innerHTML * 10 + value;
+    }
 };
+
+function onClickC () {
+    screen.innerHTML = Math.floor(screen.innerHTML / 10);
+}
